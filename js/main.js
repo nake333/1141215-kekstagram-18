33942d280a05d3879f820c55ab26ee56405fd698
unknown <sfilippov333@gmail.com>
2019-09-23 17:24:26 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var COMMENTS_ARRAY = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var COMMENTS_NAMES = ['Кирилл', 'Виктория', 'Елизавета', 'Александр', 'Максим', 'Юлия', 'Ева'];
 var MIN_LIKES = 15;
 var MAX_LIKES = 250;
 var PHOTO_COUNT = 25;
@@ -14,14 +15,33 @@ var getRandomNumber = function (min, max) {
   return Math.floor((Math.random() * (max - min + 1)) + min);
 };
 
-var getPhotoObjects = function (minLikes, maxLikes, commentsArray, photoCount) {
+var getRandomComments = function () {
+  var randomComments = getRandomNumber(1, 2);
+  var randomElement = COMMENTS_ARRAY[getRandomNumber(0, COMMENTS_ARRAY.length)];
+  return (randomComments === 1) ? randomElement : randomElement + ' ' + randomElement;
+};
+
+var getArrayComments = function () {
+  var arr = [];
+  var arrLength = getRandomNumber(1, 5);
+  for (var i = 0; i < arrLength; i++) {
+    arr[i] = {
+      avatar: 'img/avatar-' + (i + 1) + '.svg',
+      message: getRandomComments(),
+      name: COMMENTS_NAMES[getRandomNumber(0, COMMENTS_NAMES.length)]
+    };
+  }
+  return arr;
+};
+
+var getPhotoObjects = function () {
   var photoSetup = [];
 
   for (var i = 0; i < photoCount; i++) {
     var tempObject = {};
     tempObject.url = 'photos/' + (i + 1) + '.jpg';
     tempObject.likes = getRandomNumber(minLikes, maxLikes);
-    tempObject.comments = getRandomNumber(1, commentsArray.length);
+    tempObject.comments = getRandomComments();
     photoSetup.push(tempObject);
   }
 
@@ -38,7 +58,7 @@ var getPhotoTemplate = function (photo) {
   return photoElement;
 };
 
-var renderPhotos = function (minLikes, maxLikes, commentsArray, photoCount) {
+var renderPhotos = function () {
   var fragment = document.createDocumentFragment();
   var object = getPhotoObjects(minLikes, maxLikes, commentsArray, photoCount);
 
